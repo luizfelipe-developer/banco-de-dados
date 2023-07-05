001085a711cf8f5a537575c829e6d274946c7ecd
Felipe-Dev <felipe-developer@outlook.com>
2023-07-05 15:04:56 -0300

    Update verifica_login.php

--- login/php/verifica_login.php
+++ login/php/verifica_login.php
@@ -5,9 +5,15 @@
         $login = $_POST['login'];
         $senha = $_POST['senha'];
         echo "Usuário: ".$login; 
-        echo "<br>"
-        
+        echo "<br>";
         echo "Senha: ".$senha; 
+        
+        $sql = "SELECT `usuario` FROM `usuarios` WHERE `usuario`='$login'";
+        $resultado = $conexao -> query($sql);
+        $qtd = mysqli_num_rows($resultado);
+        echo "<br>";
+
+        echo "qtd: ".$qtd;
     }
 
 ?>
