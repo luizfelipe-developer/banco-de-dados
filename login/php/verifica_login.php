b26975e07ffe7f11e6e53d21f69f070120c9db38
Felipe-Dev <felipe-developer@outlook.com>
2023-07-05 17:55:33 -0300

    main

--- login/php/verifica_login.php
+++ login/php/verifica_login.php
@@ -1,6 +1,6 @@
 <?php 
     require_once 'conexao.php';
-
+    session_start(); //inicializa a sessao
     if (isset($_POST['bt-entrar'])) {
         $login = $_POST['login'];
         $senha = $_POST['senha'];
@@ -10,10 +10,19 @@
         
         $sql = "SELECT `usuario` FROM `usuarios` WHERE `usuario`='$login'";
         $resultado = $conexao -> query($sql);
-        $qtd = mysqli_num_rows($resultado);
-        echo "<br>";
+      //  $qtd = mysqli_num_rows($resultado);
+      //  echo "<br>";
 
-        echo "qtd: ".$qtd;
+       // echo "qtd: ".$qtd;
+        if (mysqli_num_rows($resultado)>0) {
+            $sql= "SELECT * FROM `usuarios` WHERE `usuario`='$login' AND `senha`='$senha'";
+
+            $resultado = $conexao -> query($sql);
+            if (mysqli_num_rows($resultado)==1){
+                header('Location:../pages/pagina.html');
+            }
+            echo "<br><br> Usuário ou senha estão incorretos.";
+        }
     }
 
 ?>
