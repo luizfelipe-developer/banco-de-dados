94e613a172f06435a5b933b6f4e8f42941e22e60
Felipe-Dev <felipe-developer@outlook.com>
2023-07-10 17:22:07 -0300

    medico

---
+++ bd-hospital/admin/php/cadastro-paciente.php
@@ -0,0 +1,22 @@
+<?php 
+include("conexao.php");
+//receber as iformações repassadas pelo método POST pelo formulário
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+$crm = $_POST['crm'];
+$endereco = $_POST['endereco'];
+$cidade = $_POST['cidade'];
+$especialidade = $_POST['especialidade'];
+//inserir os valores adicionados das variáveis nos campos da tabela cliente do BD
+$inserirSql = "INSERT INTO paciente(crm, nome, sobrenome, endereco, cidade, especialidade) VALUES ('$crm', '$nome', '$sobrenome', '$endereco', '$cidade', '$especialidade')";
+//sempre que os valores forem do tipo varchar, devem ficar entre 'aspas simples'
+//Verificação
+if (mysqli_query($conexao, $inserirSql)) {
+    echo "Usuário cadastrado!";
+} else {
+    echo "Usuário não cadastrado. Erro: ".mysqli_connect_error($conexao);
+}
+//encerrar a conexão, para evitar travamentos no BD
+mysqli_close($conexao);
+header('Location: ../../index.html');
+?>
