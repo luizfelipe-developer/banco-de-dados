28f527bee687e252063b95935971a1fa75839f4b
Felipe-Dev <felipe-developer@outlook.com>
2023-08-25 02:04:41 -0300

    treinos

--- academia/assets/php/tabelaTreinos.php
+++ academia/assets/php/tabelaTreinos.php
@@ -1,75 +1,157 @@
+
 <?php
+    session_start();
+    //Verificar se a sessâo "online" foi criada
+    
     include "conexao.php";
+    
     
     if(mysqli_connect_error())
     trigger_error(mysqli_connect_error());
 
         $sql = "SELECT * FROM `treinos` ";
-        $query = $conexao->query($sql);
-
+         $query = $conexao->query($sql);
+         $user = $_SESSION['nomeUse'];
+        
         $msg = $query->num_rows;
+        
+    if(!isset($_SESSION['online'])){
     
+        header('Location: index.php');
+    }else {
+        $user = $_SESSION['nomeUsu'];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../cadastro/css/table.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
-        <title>Lista de Alunos</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>VISUALIZAR TREINOS</title>
+    <link rel="stylesheet" href="../../css/header.css">
+    <link rel="shortcut icon" href="../../img/logo-icon.png" type="image/x-icon">
+
+    <style>
+        body{
+            background-image: url(../../img/academia-darkk.jpg);
+            background-size: cover;
+            background-repeat: no-repeat;
+            
+            color: white;
+            text-align: center;
+        }
+        .table-bg{
+            width: 80%;
+            margin: 0 auto;
+    background-color: #fff;       
+     border-radius: 15px 15px 0 0;
+        }
+
+        th{
+        color: black;
+        }
+
+        td{
+        color: black;
+        }
+
+        h1{
+        margin-top: 100px;
+        }
+
+        .box-search{
+            display: flex;
+            justify-content: center;
+            gap: .1%;
+        }
+    </style>
 </head>
 <body>
-    <div class="container">
-        <div id="msg">
-            <span>Registros encontrados:  <?php echo "$msg"?></span>
+<header>
+        <div id="teste">
+          <div class="logo-imagem">
+            <a href="../../index.html">
+              <picture>
+                <source  media="(max-width:600px )" class="logo-icon" srcset="../../img/logo-icon.png" type="image/png">
+              <img id="img" src="../../img/logo-academia.png" alt=""></a>
+              </picture>
+              
+        
+         </div>
         </div>
-        <table>
-            <!-- <form action="usuarios.php" method="GET">
-                <div class="input-usuario" >
-                    <label for="usuario">Quantos usuários você quer visualizar ?</label>
-                    <input type="number" name="usuario" id="usuario" min="1    ">
-                </div>
-                <div class="botao" >
-                    <button >pesquisar</button>
-                </div>
-            </form> -->
-            
+                            
+            <nav>
+                <ul class="opcoes">
+                    <!-- CHECKBOX HACK -->
+                <input type="checkbox" id="checkbox_toggle" />
+                <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+                
+                <!-- NAVEGAÇÃO MENUS -->
+                    <div class="menu">
+              
+                        <li><a href="/academia/professor/treinos/registros.html">VOLTAR</a></li> 
+                    </div>
+                </ul>
+                
+              </nav>
+              
+    </header>
+    <br>
+    <?php
+        echo "<h1>Bem vindo <u>$user</u></h1>";
+    ?>
+    
+    <div class="m-5">
+        <table class="table text-white table-bg">
             <thead>
-                <tr id="cabecalho" >
-                    <td class = "celula" >CÓDIGO Aluno</td>
-                    <td class = "celula" >CÓDIGO Personal</td>
-                    <td class = "celula" >NOME</td>
-                    <td class = "celula" >SEGUNDA</td>
-                    <td class = "celula" >TERÇA</td>
-                    <td class = "celula" >QUARTA</td>
-                    <td class = "celula" >QUINTA</td>
-                    <td class = "celula" >SEXTA</td>
-                    <td class = "celula" >SÁBADO</td>
+                <tr>
+                <th scope="col">COD_ALUNO</th>
+                <th scope="col">COD_PERSONAL</th>
+                <!-- <th scope="col">NOME</th> -->
+                    <th scope="col">SEGUNDA</th>
+                    <th scope="col">TERÇA</th>
+                    <th scope="col">QUARTA</th>
+                    <th scope="col">QUINTA</th>
+                    <th scope="col">SEXTA</th>
+                    <th scope="col">SÁBADO</th>
+                    <th scope="col">...</th>
                 </tr>
             </thead>
             <tbody>
-                <?php while ($dados_treino = $query->fetch_array()) { ?>        
-                  
-                <tr>
-                    <td> <?php echo $dados_treino['cod_aluno'];?></td>
-                    <td> <?php echo $dados_treino['cod_personal'];?></td>
-                    <td id = "td-nome" > <?php echo $dados_treino['nome'];?></td>
-                    <td> <?php echo $dados_treino['segunda'];?></td>
-                    <td> <?php echo $dados_treino['terca'];?></td>
-                    <td> <?php echo $dados_treino['quarta'];?></td>
-                    <td> <?php echo $dados_treino['quinta'];?></td>
-                    <td> <?php echo $dados_treino['sexta'];?></td>
-                    <td> <?php echo $dados_treino['sabado'];?></td>
-                    </tr>
-               <?php } ?>
+         
+                      <?php while ($user_data = $query->fetch_array()) {
+                        echo "<tr>";
+                        echo "<td>".$user_data['cod_aluno']."</td>";
+                        echo "<td>".$user_data['cod_personal']."</td>";
+                        // echo "<td>".$user_data['nome']."</td>";
+                        echo "<td>".$user_data['segunda']."</td>";
+                        echo "<td>".$user_data['terca']."</td>";
+                        echo "<td>".$user_data['quarta']."</td>";
+                        echo "<td>".$user_data['quinta']."</td>";
+                        echo "<td>".$user_data['sexta']."</td>";
+                        echo "<td>".$user_data['sabado']."</td>";
+                        echo "<td>
+                        <a class='btn btn-sm btn-primary' href='editarTreino.php?cod_aluno=$user_data[cod_aluno]' title='Editar'>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                                <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+                            </svg>
+                            </a> 
+                            <a class='btn btn-sm btn-danger' href='deleteTreino.php?cod_aluno=$user_data[cod_aluno]' title='Deletar'>
+                                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
+                                    <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
+                                </svg>
+                            </a>
+                            </td>";
+                        echo "</tr>";
+                    }
+                    ?>
+
+
+
             </tbody>
         </table>
-    </div>    
+    </div>
 </body>
 </html>
-
-
-
